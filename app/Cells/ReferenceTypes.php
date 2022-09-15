32991b88fa393225f2227b4354459430d5c9c566
Genercode <72575486+autocode2020@users.noreply.github.com>
2022-09-15 18:40:12 +0100

    Create ReferenceTypes.php

---
+++ app/Cells/ReferenceTypes.php
@@ -0,0 +1,11 @@
+<?php
+namespace GenerCodeOrm\Cells;
+
+abstract class ReferenceTypes
+{
+    const PRIMARY = 0;
+    const PARENT = 1;
+    const OWNER = 2;
+    const REFERENCE = 3;
+    const RECURSIVE = 4;
+}
