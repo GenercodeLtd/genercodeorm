224ede6b3877080a270d3418849a129773c06929
Genercode <72575486+autocode2020@users.noreply.github.com>
2022-09-23 07:32:35 +0100

    Update RepoTest.php

--- tests/RepoTest.php
+++ tests/RepoTest.php
@@ -14,32 +14,12 @@ use Illuminate\Container\Container as Container;
 require_once(__DIR__ . "/../app/standardfunctions.php");
 \GenerCodeOrm\regAutoload("GenerCodeOrm", __DIR__ . "/../app");
 
-<<<<<<< Updated upstream
-require(__DIR__ . "/testproject/Fields.php");
-require(__DIR__ . "/testproject/Models.php");
-require(__DIR__ . "/testproject/Projects.php");
-require(__DIR__ . "/testproject/Sections.php");
-require(__DIR__ . "/testproject/States.php");
-require(__DIR__ . "/testproject/Profiles.php");
-require(__DIR__ . "/testproject/SchemaFactory.php");
-=======
-require_once(__DIR__ . "/testproject/Fields.php");
-require_once(__DIR__ . "/testproject/Models.php");
-require_once(__DIR__ . "/testproject/Projects.php");
-require_once(__DIR__ . "/testproject/Sections.php");
-require_once(__DIR__ . "/testproject/States.php");
-require_once(__DIR__ . "/testproject/SchemaFactory.php");
->>>>>>> Stashed changes
-
-final class RepoTest extends TestCase
-{
-
+final class RepoTest extends TestCase {
 
     protected $dbmanager;
 
     public function setUp(): void
     {
-        $container = new Container();
         $capsule = new Capsule($container);
         $capsule->addConnection([
             'driver'    => 'mysql',
@@ -56,95 +36,6 @@ final class RepoTest extends TestCase
     }
 
 
-<<<<<<< Updated upstream
-    public function testGet() {
-        $profile = new GenerCodeOrm\Profile();
-        $profile->id = 1;
-        $profile->type = "test";
-
-        $profile->models = ["models"=>["perms"=>["get"], "is_owner"=>false]];
-
-        $factory = new SchemaFactory();
-
-        $repo = new GenerCodeOrm\Repository(
-            $this->dbmanager,
-            $factory,
-            $profile
-        );
-        
-       
-        $res = $repo->get("models", ["name"=>"profiles"]);
-    
-        
-        $this->assertSame("profiles", $res->name);
-    }
-
-
-    public function testGetAll() {
-        $profile = new GenerCodeOrm\Profile();
-        $profile->id = 1;
-        $profile->type = "test";
-
-        $profile->models = ["models"=>["perms"=>["get"], "is_owner"=>false]];
-
-        $factory = new SchemaFactory();
-
-        $repo = new GenerCodeOrm\Repository(
-            $this->dbmanager,
-            $factory,
-            $profile
-        );
-        
-       
-        $res = $repo->getAll("models");
-
-        $this->assertGreaterThan(1, count($res));
-    }
-
-
-    public function testLimit() {
-        $profile = new GenerCodeOrm\Profile();
-        $profile->id = 1;
-        $profile->type = "test";
-
-        $profile->models = ["models"=>["perms"=>["get"], "is_owner"=>false]];
-
-        $factory = new SchemaFactory();
-
-        $repo = new GenerCodeOrm\Repository(
-            $this->dbmanager,
-            $factory,
-            $profile
-        );
-        
-        $repo->limit  = 3;
-        $res = $repo->getAll("models");
-
-        $this->assertSame(3, count($res));
-    }
-
-
-    public function testChildren() {
-        $profile = new GenerCodeOrm\Profile();
-        $profile->id = 1;
-        $profile->type = "test";
-
-        $profile->models = ["models"=>["perms"=>["get"], "is_owner"=>false]];
-
-        $factory = new SchemaFactory();
-
-        $repo = new GenerCodeOrm\Repository(
-            $this->dbmanager,
-            $factory,
-            $profile
-        );
-
-        $repo->children = ["fields", "sections"];
-        
-        $res = $repo->getAll("models");
-
-        $this->assertSame(3, count($res));
-=======
     public function testRepo() {
         $factory = new SchemaFactory();
         $repo = new GenerCodeOrm\Repository($this->dbmanager, $factory);
@@ -157,7 +48,6 @@ final class RepoTest extends TestCase
         $res = $repo->getAll();
         $id = $res["--id"];
         $this->assertNotSame(0, $id);
->>>>>>> Stashed changes
     }
 
 
