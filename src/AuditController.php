6f2c88981616e782fd348e9a855def9a37b7e284
Genercode <72575486+autocode2020@users.noreply.github.com>
2022-11-04 14:26:06 +0000

    Update AuditController.php

--- src/AuditController.php
+++ src/AuditController.php
@@ -9,18 +9,30 @@ class AuditController extends AppController
 {
     //audit functions
 
-
     public function getObjectAt($name, $id, $last_published)
     {
         $this->checkPermission($name, "get");
 
-        $ctrl = $this->app->get(ModelController::class);
-        $vals = $ctrl->get("audit", new Fluent([
-            "--created" => [ "min"=>$last_published],
-            "model"=>$name,
-            "model-id"=>$id,
-            "__order"=>["--created"=>"DESC"]]));
+        $model = $this->model("audit");
 
+
+        $where = new InputSet("audit");
+        $where->addData("--model-id", $id);
+        $where->addData("model", $name);
+        $where->addData("--created", ["min"=>$last_published]);
+      
+        $dataSet = new DataSet($model);
+        $dataSet->data($where);
+        $dataSet->validate();
+
+        $model->filterBy($dataSet);
+
+        $orderSet = new InputSet("audit");
+        $orderSet->data(["--created"=>"ASC"]);
+        $model->order($orderSet);
+
+        $vals = $model->setFromEntity()->get()->toArray();
+       
         if (count($vals) == 0) return new \StdClass;
 
         if ($vals[count($vals - 1)]->action == "POST") return new \StdClass;
