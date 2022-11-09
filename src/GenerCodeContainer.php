b3c5bb3ae3c6c522d2ccce8be9e8d12e9c7ff6a4
Genercode <72575486+autocode2020@users.noreply.github.com>
2022-11-09 11:08:58 +0000

    reducing boilerplate

--- src/GenerCodeContainer.php
+++ src/GenerCodeContainer.php
@@ -1,36 +1,17 @@
 <?php
 namespace GenerCodeOrm;
 
-use \Illuminate\Support\Fluent;
 use \Illuminate\Container\Container;
-use \Illuminate\Database\Connectors\ConnectionFactory;
-use \Illuminate\Database\DatabaseManager;
-use \Illuminate\FileSystem\FileSystemManager;
-use Psr\Log\LoggerInterface;
-
 
 class GenerCodeContainer extends Container {
 
-     
-    function bindCommonDependencies(Fluent $configs) {
-
-        $this->instance('config', $configs);
-
-        $manager = new DatabaseManager($this, new ConnectionFactory($this));
-        $this->instance(\Illuminate\Database\Connection::class, $manager->connection());
-
-
+    function __construct() {
+        parent::__construct();
         $this->instance(Container::class, $this);
+    }
 
-
-        $this->bind(\GenerCodeOrm\FileHandler::class, function($app) {
-
-            $file = new \Illuminate\Filesystem\FilesystemManager($app);
-            $disk = $file->disk($this->config["filesystems.default"]);
-            $fileHandler = new \GenerCodeOrm\FileHandler($disk);
-            return $fileHandler;
-        });
- 
+    function bindConfigs(\Illuminate\Contracts\Config $configs) {
+        $this->instance(\Illuminate\Contracts\Config::class, $configs);
     }
 
 
