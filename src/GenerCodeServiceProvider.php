6f7ee32b6a700e0bd6ecd8577eaa61f50b1ce94e
Genercode <72575486+autocode2020@users.noreply.github.com>
2022-11-29 15:20:37 +0000

    moving to service provider

--- src/GenerCodeServiceProvider.php
+++ src/GenerCodeServiceProvider.php
@@ -5,16 +5,20 @@ use \Illuminate\Container\Container;
 use \Illuminate\Database\Connectors\ConnectionFactory;
 use \Illuminate\Database\DatabaseManager;
 use \Illuminate\Auth\AuthManager;
+use Illuminate\Support\ServiceProvider;
 
-class GenerCodeContainer extends Container {
+class GenerCodeServiceProvider extends ServiceProvider {
 
-    function __construct() {
-        $this->instance(Container::class, $this);
 
+    function register() {
         
-        $this->singleton(\Illuminate\Database\Connection::class, function($app) {
-            $manager = new DatabaseManager($app, new ConnectionFactory($app));
-            return $manager->connection();
+        
+        $this->bind(\Illuminate\Database\Connection::class, function($app) {
+            return $this->app["db"]->connection();
+        });
+
+        $this->instance("db", function($app) {
+            return new DatabaseManager($app, new ConnectionFactory($app));
         });
 
         $this->bind(\Illuminate\Filesystem\FilesystemManager::class, function($app) {
@@ -25,19 +29,19 @@ class GenerCodeContainer extends Container {
         $this->bind(\Illuminate\Auth\DatabaseUserProvider::class, function ($app) {
             return new \Illuminate\Auth\DatabaseUserProvider($this->make(\Illuminate\Database\Connection::class, Hasher, "users"));
         });
-    }
 
-    function bindConfigs(\Illuminate\Config\Repository $configs) {
-        $this->instance("config", $configs);
     }
 
 
-    function bindUserDependencies() {
 
+    function boot() {
+        if (!$this->app->config->has("factory")) {
+            throw new \PtjException("Factory needs to be set in the configs");
+        }
         $factory_name = $this->config->get("factory");
         $factory = new $factory_name();
 
-        $auth = $this->get(AuthManager::class);
+        $auth = $this->get("auth");
         $user = $auth->user();
 
         if (!$user) {
