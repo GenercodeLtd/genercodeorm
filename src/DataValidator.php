3dc65cfdeb97f9a9059d1d7b5f852c1a14ae0b37
Genercode <72575486+autocode2020@users.noreply.github.com>
2023-01-23 15:47:57 +0000

    Create DataValidator.php

---
+++ src/DataValidator.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace GenerCodeOrm;
+
+class DataValidator {
+
+
+    protected function validateChild(string $name, ?Binds\Bind $parent = null) {
+        $model= $this->model($name);
+        $model->select($model->raw("count(" . $name . ") as 'count'"))
+        ->setFromEntity()
+        ->take(1);
+        if ($parent) {
+            $model->filter($parent);
+        }
+        return  $model->get()->first()->count;
+    }  
+
+
+    protected function getEntity($name) {
+        return app()->get("factory")->create($name);
+    }
+
+
+    public function validate(string $name = null, int $parent_id = 0)
+    {
+        $this->checkPermission($name, "get");
+
+        $children = [];
+        $bind = null;
+        if ($name) {
+            $entity = $this->getEntity($name);
+            $children = $entity->get("--id")->reference;
+            $bind = new Binds\Bind($entity->get("--parent"));
+            $bind->value = $parent_id;
+        } else {
+            $children = app()->get("factory")::getRootEntities();
+        }
+
+        $checks = [];
+        foreach($children as $slug) {
+            $child_entity = $this->getEntity($slug);
+            if ($child_entity->min_rows OR $child_entity->max_rows) {
+
+                $checks[$slug] = $this->validateChild($slug, $bind);
+            } 
+        }
+
+        return $checks;
+    }
+}
